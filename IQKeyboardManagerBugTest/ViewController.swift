ffd747a988d340c59ac09411c762cf44e2b682af
Jim Boulter <jiboulte@microsoft.com>
2018-10-04 09:46:41 -0700

    Initial Commit

---
+++ IQKeyboardManagerBugTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IQKeyboardManagerBugTest
+//
+//  Created by Jim Boulter on 10/4/18.
+//  Copyright © 2018 Jim Boulter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
